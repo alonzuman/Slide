3697fe7f0e286eaeca13ed7e5eaf56c500f3504b
Alon Zuman <alonzuman7@gmail.com>
2021-04-27 19:31:35 +0300

    tiny fix for stream header

--- src/scenes/Stream/StreamHeader.tsx
+++ src/scenes/Stream/StreamHeader.tsx
@@ -11,37 +11,19 @@ import useStreamLayout from '../../hooks/useStreamLayout'
 import useStream, { useStreamActiveSpeaker, useStreamID, useStreamIsJoined, useStreamMeta, useStreamOnStage, useStreamSpeakers } from '../../hooks/useStream'
 import Entypo from 'react-native-vector-icons/Entypo'
 import { useUser } from '../../hooks/useUser'
-import BlurWrapper from '../../core/BlurWrapper'
+import StreamHeaderSpeakers from './StreamHeaderSpeakers'
 
 export default function StreamHeader() {
   const { setOptions, goBack } = useNavigation()
-  const insets = useSafeAreaInsets()
-  const { openModal, layout } = useStreamLayout()
   const { updateClientRole } = useStream()
   const { user } = useUser()
-  const left = useState(new Animated.Value(0))[0]
-
   const isJoined = useStreamIsJoined()
   const streamID = useStreamID()
   const onStage = useStreamOnStage()
   const meta = useStreamMeta()
-  const activeSpeaker = useStreamActiveSpeaker()
-  const speakers = useStreamSpeakers()
   const isSpeaker = onStage?.includes(user?._id)
-  const activeSpeakerData = speakers?.find(v => v?.streamID === activeSpeaker)
 
   console.log('re-rendered header', user?.name)
-
-  const slideLeft = () => {
-    Animated.spring(left, {
-      toValue: layout?.isZenMode ? -256 : 0,
-      useNativeDriver: false
-    }).start()
-  }
-
-  useEffect(() => {
-    slideLeft()
-  }, [layout?.isZenMode])
 
   useEffect(() => {
     // Checks if current user is a speaker and if so, pushes it to the speakers and changes role
@@ -57,34 +39,7 @@ export default function StreamHeader() {
     })
   }, [setOptions, isJoined, meta?.name, streamID])
 
-  if (!activeSpeaker) return null
-
-  const _renderPrimary = () => {
-    if (activeSpeakerData?.name) return `${activeSpeakerData?.name} ${speakers?.length > 1 ? `and ${speakers?.length - 1} more` : ''}`
-    return `${speakers?.length} Speakers`
-  }
-
   return (
-    <Animated.View style={{ zIndex: 9, marginLeft: left }}>
-      <BlurWrapper
-        style={{
-          alignSelf: 'flex-start',
-          borderRadius: 24,
-          zIndex: 9,
-          marginTop: insets.top + 40,
-          marginLeft: 12,
-        }}
-      >
-        <ListItem
-          style={{ padding: 4 }}
-          onPress={() => openModal(Constants.StreamModals.ON_STAGE)}
-          renderBefore={<Avatar style={{ marginRight: -4 }} size='m' uri={activeSpeakerData?.avatar} />}
-          primary={_renderPrimary()}
-          primaryStyle={{ color: '#fff' }}
-          labelStyle={{ color: '#fff' }}
-          label='On Stage 🎙️'
-        />
-      </BlurWrapper>
-    </Animated.View>
+    <StreamHeaderSpeakers />
   )
 }
---
+++ src/scenes/Stream/StreamHeaderSpeakers.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react'
+import { Animated } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import Constants from '../../constants/Constants'
+import Avatar from '../../core/Avatar'
+import BlurWrapper from '../../core/BlurWrapper'
+import ListItem from '../../core/ListItem'
+import { useStreamActiveSpeaker, useStreamSpeakers } from '../../hooks/useStream'
+import useStreamLayout from '../../hooks/useStreamLayout'
+
+export default function StreamHeaderSpeakers() {
+  const insets = useSafeAreaInsets()
+  const { layout, openModal } = useStreamLayout()
+  const activeSpeaker = useStreamActiveSpeaker()
+  const speakers = useStreamSpeakers()
+  const activeSpeakerData = speakers?.find(v => v?.streamID === activeSpeaker)
+  const left = useState(new Animated.Value(0))[0]
+
+  useEffect(() => {
+    slideLeft()
+  }, [layout?.isZenMode])
+
+  const slideLeft = () => {
+    Animated.spring(left, {
+      toValue: layout?.isZenMode ? -256 : 0,
+      useNativeDriver: false
+    }).start()
+  }
+
+  const _renderPrimary = () => {
+    if (activeSpeakerData?.name) return `${activeSpeakerData?.name} ${speakers?.length > 1 ? `and ${speakers?.length - 1} more` : ''}`
+    return `${speakers?.length} Speakers`
+  }
+
+  if (!activeSpeaker) return null
+
+  return (
+    <Animated.View style={{ zIndex: 9, marginLeft: left }}>
+      <BlurWrapper
+        style={{
+          alignSelf: 'flex-start',
+          borderRadius: 24,
+          zIndex: 9,
+          marginTop: insets.top + 40,
+          marginLeft: 12,
+        }}
+      >
+        <ListItem
+          style={{ padding: 4 }}
+          onPress={() => openModal(Constants.StreamModals.ON_STAGE)}
+          renderBefore={<Avatar style={{ marginRight: -4 }} size='m' uri={activeSpeakerData?.avatar} />}
+          primary={_renderPrimary()}
+          primaryStyle={{ color: '#fff' }}
+          labelStyle={{ color: '#fff' }}
+          label='On Stage 🎙️'
+        />
+      </BlurWrapper>
+    </Animated.View>
+  )
+}
